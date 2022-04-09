b8fbeabdd4fce50e6c8fad1b4d03df08ea25058f
Bruno Raphael Cabral de Mesquita <brcmesquita@gmail.com>
2022-04-09 16:40:34 -0300

    commit inicial

---
+++ 02 - Preparando ambiente de desenvolvimento/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Bem-vindo ao C#!");
+        }
+    }
+}
